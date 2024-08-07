a8dcab07bb04df2b55738fee98f8e20bda4787c1
vinay0000 <vinay.rrgl@gmail.com>
2024-08-07 02:07:34 -0700

    update instrument module

--- src/eose/instruments.py
+++ src/eose/instruments.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic import BaseModel, Field, model_validator
 from enum import Enum
 
+# Ensure the import path for Quaternion is correct
 from eose.utils import Quaternion
 
 class Shape(str, Enum):
@@ -9,8 +10,8 @@ class Shape(str, Enum):
     RECTANGULAR = "RECTANGULAR"
 
 class SphericalGeometry(BaseModel):
-    """  Class to handle spherical geometries (spherical circle or rectangular) which define an closed angular space of interest.
-         The pointing axis is fixed to the sensor Z-axis. 
+    """Class to handle spherical geometries (spherical circle or rectangular) which define a closed angular space of interest.
+       The pointing axis is fixed to the sensor Z-axis. 
     """
     shape: Shape = Field(Shape.CIRCULAR, description="Shape of the spherical geometry.")
     diameter: Optional[float] = Field(None, gt=0, lt=180, description="Angular diameter of circular geometry about the sensor Z-axis in degrees.")
@@ -47,11 +48,14 @@ class BasicSensor(BaseModel):
     mass: Optional[float] = Field(None, gt=0, description="Mass of the sensor in kilograms.")
     volume: Optional[float] = Field(None, gt=0, description="Volume of the sensor in cubic meters.")
     power: Optional[float] = Field(None, gt=0, description="(Average) Power consumption of the sensor in watts.")
-    orientation: Optional[Quaternion] = Field(
-        [0, 0, 0, 1],
-        description="Orientation of the instrument view, relative to the body-fixed frame.",
+    orientation: Quaternion = Field(
+        default_factory=lambda: list([0, 0, 0, 1]),
+        description="Orientation of the instrument view, relative to the spacecraft body-fixed frame.",
     )
-    field_of_view: SphericalGeometry = Field(SphericalGeometry(diameter=30), description="Field of view of the sensor.")
+    field_of_view: SphericalGeometry = Field(
+        default_factory=lambda: SphericalGeometry(diameter=30), 
+        description="Field of view of the sensor."
+    )
     data_rate: Optional[float] = Field(None, gt=0, description="Data rate of the sensor in megabits per second.")
     bits_per_pixel: Optional[int] = Field(None, gt=1, description="Bits per pixel for the sensor's data output.")
 
