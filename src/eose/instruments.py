8657250d20745efe0d1eb4a63c529a22a28d1017
vinay0000 <vinay.rrgl@gmail.com>
2024-08-08 11:29:16 -0700

    spherical geometry revised to be indicated in distinct objects per geometric config

--- src/eose/instruments.py
+++ src/eose/instruments.py
@@ -1,46 +1,24 @@
-from typing import Optional
+from typing import Optional, Literal, Union
 from pydantic import BaseModel, Field, model_validator
 from enum import Enum
 
 # Ensure the import path for Quaternion is correct
 from eose.utils import Quaternion
 
-class Shape(str, Enum):
-    CIRCULAR = "CIRCULAR"
-    RECTANGULAR = "RECTANGULAR"
-
-class SphericalGeometry(BaseModel):
-    """Class to handle spherical geometries (spherical circle or rectangular) which define a closed angular space of interest.
+class CircularGeometry(BaseModel):
+    """Class to handle spherical circular geometries which define a closed angular space of interest.
        The pointing axis is fixed to the sensor Z-axis. 
     """
-    shape: Shape = Field(Shape.CIRCULAR, description="Shape of the spherical geometry.")
-    diameter: Optional[float] = Field(None, gt=0, lt=180, description="Angular diameter of circular geometry about the sensor Z-axis in degrees.")
+    type: Literal["CircularGeometry"] = Field("CircularGeometry")
+    diameter: float = Field(..., gt=0, lt=180, description="Angular diameter of circular geometry about the sensor Z-axis in degrees.")
+
+class RectangularGeometry(BaseModel):
+    """Class to handle spherical rectangular geometries which define a closed angular space of interest.
+       The pointing axis is fixed to the sensor Z-axis. 
+    """
+    type: Literal["RectangularGeometry"] = Field("RectangularGeometry")
     angle_height: Optional[float] = Field(None, gt=0, lt=180, description="Angular height (about the sensor X-axis) of the rectangular geometry in degrees.")
     angle_width: Optional[float] = Field(None, gt=0, lt=180, description="Angular width (about the sensor Y-axis) of the rectangular geometry in degrees.")
-    
-    class Config:
-        use_enum_values = True  # This will make the enum values used directly instead of their names
-
-    @model_validator(mode='before')
-    def check_geometry_consistency(cls, values):
-        shape = values.get('shape')
-        diameter = values.get('diameter')
-        angle_height = values.get('angle_height')
-        angle_width = values.get('angle_width')
-        
-        if shape == Shape.CIRCULAR:
-            if diameter is None:
-                raise ValueError('CIRCULAR shape must have a diameter.')
-            if angle_height is not None or angle_width is not None:
-                raise ValueError('CIRCULAR shape cannot have angle_height or angle_width.')
-
-        elif shape == Shape.RECTANGULAR:
-            if angle_height is None or angle_width is None:
-                raise ValueError('RECTANGULAR shape must have both angle_height and angle_width.')
-            if diameter is not None:
-                raise ValueError('RECTANGULAR shape cannot have a diameter.')
-
-        return values
 
 class BasicSensor(BaseModel):
     name: Optional[str] = Field(None, description="Sensor name.")
@@ -52,8 +30,8 @@ class BasicSensor(BaseModel):
         default_factory=lambda: list([0, 0, 0, 1]),
         description="Orientation of the instrument view, relative to the spacecraft body-fixed frame.",
     )
-    field_of_view: SphericalGeometry = Field(
-        default_factory=lambda: SphericalGeometry(diameter=30), 
+    field_of_view: Union[CircularGeometry, RectangularGeometry] = Field(
+        default_factory=lambda: CircularGeometry(diameter=30), 
         description="Field of view of the sensor."
     )
     data_rate: Optional[float] = Field(None, gt=0, description="Data rate of the sensor in megabits per second.")
